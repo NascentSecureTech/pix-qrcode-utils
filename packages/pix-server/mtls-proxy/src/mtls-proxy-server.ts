ac7b3daf0e991d7e4bdcea7b9d5012754d7ea773
Sean Michael Wykes <sean.wykes@nascent.com.br>
2021-07-29 23:19:12 -0300

    Removed credentials (test)

--- packages/pix-server/mtls-proxy/src/mtls-proxy-server.ts
+++ packages/pix-server/mtls-proxy/src/mtls-proxy-server.ts
@@ -37,51 +37,21 @@ const agent = new https.Agent({
 			passphrase: "",
 		});*/
 
-let hdrs = {
-  Authorization: "Basic ZXlKcFpDSTZJbU15TVRVd1lqRXRZV1EwTXkwMFkyUXdMVGt3TmpndE5pSXNJbU52WkdsbmIxQjFZbXhwWTJGa2IzSWlPakFzSW1OdlpHbG5iMU52Wm5SM1lYSmxJam94T1RjeE1Dd2ljMlZ4ZFdWdVkybGhiRWx1YzNSaGJHRmpZVzhpT2pGOTpleUpwWkNJNklqbGpJaXdpWTI5a2FXZHZVSFZpYkdsallXUnZjaUk2TUN3aVkyOWthV2R2VTI5bWRIZGhjbVVpT2pFNU56RXdMQ0p6WlhGMVpXNWphV0ZzU1c1emRHRnNZV05oYnlJNk1Td2ljMlZ4ZFdWdVkybGhiRU55WldSbGJtTnBZV3dpT2pFc0ltRnRZbWxsYm5SbElqb2lhRzl0YjJ4dloyRmpZVzhpTENKcFlYUWlPakUyTWpjME9ERTJPRGN5T0RSOQ==",
-  "content-type": "application/x-www-form-urlencoded"
-}
-
-let req: ProxyRequest = {
-  method: "POST",
-  url: "https://oauth.hm.bb.com.br/oauth/token",
-  body: "grant_type=client_credentials",
-  headers: JSON.stringify( hdrs )
-}
-
-let p = new URLSearchParams();
-p.append("grant_type","client_credentials");
-
-var postParams = {
-		method: 'POST',
-		headers: new Headers( JSON.parse( req.headers ) ),
-//		httpsAgent: agent,
-		body: p
-	};
-
-
-/*axios.post(req.url, p, { headers: {
-  Authorization: "Basic ZXlKcFpDSTZJbU15TVRVd1lqRXRZV1EwTXkwMFkyUXdMVGt3TmpndE5pSXNJbU52WkdsbmIxQjFZbXhwWTJGa2IzSWlPakFzSW1OdlpHbG5iMU52Wm5SM1lYSmxJam94T1RjeE1Dd2ljMlZ4ZFdWdVkybGhiRWx1YzNSaGJHRmpZVzhpT2pGOTpleUpwWkNJNklqbGpJaXdpWTI5a2FXZHZVSFZpYkdsallXUnZjaUk2TUN3aVkyOWthV2R2VTI5bWRIZGhjbVVpT2pFNU56RXdMQ0p6WlhGMVpXNWphV0ZzU1c1emRHRnNZV05oYnlJNk1Td2ljMlZ4ZFdWdVkybGhiRU55WldSbGJtTnBZV3dpT2pFc0ltRnRZbWxsYm5SbElqb2lhRzl0YjJ4dloyRmpZVzhpTENKcFlYUWlPakUyTWpjME9ERTJPRGN5T0RSOQ==",
-  "content-type": "application/x-www-form-urlencoded",
-  "accepts": "application/json",
-} } ).then( (r) => {
-  console.log( r.status == 200 );
-  console.log( r.data )
-})*/
 
 async function handleRequest( req: ProxyRequest ): Promise<ProxyResponse> {
+
   return fetch( req.url, {
     method: req.method,
     headers: new Headers( JSON.parse( req.headers ) ),
     body: req.body
     } )
     .then( async (resp) => {
-      console.log( resp.status );
-      console.log(resp.headers.raw() )
-      console.log( JSON.stringify( resp.headers.keys() ) ) //get('content-type')
+//      console.log( resp.status );
+//      console.log(resp.headers.raw() )
+//      console.log( JSON.stringify( resp.headers.keys() ) ) //get('content-type')
 
       return resp.text().then( (t) => {
-        console.log(t )
+//        console.log(t )
 
         const proxyResp: ProxyResponse = {
           status: resp.status,
